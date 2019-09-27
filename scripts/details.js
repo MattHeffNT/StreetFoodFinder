e12236fc23b02baa717f0ebcc51b509af6a6b473
MattHeffNT <37504532+MattHeffNT@users.noreply.github.com>
2019-09-27 23:48:17 +0930

    had to update JSON link, got rid of leaflet for google maps

--- scripts/details.js
+++ scripts/details.js
@@ -1,6 +1,8 @@
 function details() {
-    var url = 'https://opendata.arcgis.com/datasets/f62cbfbf11494495984097ef8ed6a8a9_0.geojson'
-        // Send request to server
+    var url = 'http://open-darwin.opendata.arcgis.com/datasets/6d6453a83bbc4ab8b7591e545dd40d65_0.geojson'
+        // 'https://opendata.arcgis.com/datasets/f62cbfbf11494495984097ef8ed6a8a9_0.geojson'
+
+    // Send request to server
     fetch(url)
         // Get JSON object from request
         .then((response) => response.json())
@@ -9,54 +11,84 @@ function details() {
 
             data.features[i].properties.Name
                 // if it matches, this specific item should be skipped
-            var vendorName = data.features[i].properties.Name
-            var vendorLocation = data.features[i].properties.Location
-            var vendorHours = data.features[i].properties.Open_Times_Description
+            var vendorName = data.features[i].properties.BusinessName
+            var vendorLocation = data.features[i].properties.SiteName
+            var vendorWeHours = data.features[i].properties.WeekendHours
+            var vendorWdHours = data.features[i].properties.WeekdaysHours
+            var vendorPhHours = data.features[i].properties.PublicHolHours
             var vendorWeb = data.features[i].properties.Website
-            var vendorLat = data.features[i].properties.CENTROID_Y
-            var vendorLong = data.features[i].properties.CENTROID_X
+            var vendorLat = data.features[i].geometry[1]
+            var vendorLong = data.features[i].geometry[0]
+            var myLatLng = { lat: vendorLat, lng: vendorLong }
 
             document.querySelector('.display-4').innerHTML = `
                 ${vendorName}
                 `
             document.getElementById('body-container').innerHTML = `<ul style="list-style:none;">
                <li><strong> Location: </strong>${vendorLocation}</li>
-                <li><strong> Opening Hours: </strong>${vendorHours}</li>
+                <li><strong> Weekend Hours: </strong>${vendorWeHours}</li>
+                <li><strong> Weekday Hours: </strong>${vendorWdHours}</li>
+                <li><strong> Public Holidy Hours: </strong>${vendorPhHours}</li>
                 
                 <li style="text-align:center;"><a class="btn btn-primary" href=${vendorWeb} role="button">Website</a></li>
                 
 
-
                 </ul>
                 `
-            navigator.geolocation.getCurrentPosition(
+
+            navigator.geolocation.watchPosition(
+
                 // get user location
                 function(position) {
+                    lat = position.coords.latitude
+                    long = position.coords.longitude
+                        // console.log(long)
+
+                    // directions API
+
+                    var directionsService = new google.maps.DirectionsService()
+                    var directionsRenderer = new google.maps.DirectionsRenderer()
+
+                    var origin = new google.maps.LatLng(lat, long) // place user location here
+                    var destination = new google.maps.LatLng(vendorLat, vendorLong) // vendor location
+
+                    var mapOptions = {
+                        zoom: 14,
+                        center: origin,
+                        disableDefaultUI: true
+                    }
+
+                    var myLatLng = { lat: lat, lng: long };
 
 
-                    latlng = new L.LatLng(position.coords.latitude, position.coords.longitude)
-                    console.log(latlng)
-                        //console.log('this is the user poisiton ' + latlng)
+
+                    var map = new google.maps.Map(document.getElementById('map'), mapOptions)
+                    directionsRenderer.setMap(map)
+
+                    var marker = new google.maps.Marker({
+                        position: myLatLng,
+                        map: map,
+                        title: 'Hello World!'
+                    })
 
 
-                    // leaflet Map
-                    var mymap = L.map('mapid').setView([vendorLat, vendorLong], 13);
-                    var markerUser = L.marker(latlng).addTo(mymap)
-                    var markerVendor = L.marker([vendorLat, vendorLong]).addTo(mymap)
-                        .bindPopup(`${vendorName}`).openPopup();
+                    // var selectedMode = Driving
 
-                    const attribution =
-                        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-                    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-                    const tiles = L.tileLayer(tileUrl, { attribution });
+                    var request = {
+                        origin: origin,
+                        destination: destination,
+                        // Note that Javascript allows us to access the constant
+                        // using square brackets and a string value as its
+                        // "property."
+                        travelMode: 'DRIVING'
+                    };
 
-                    tiles.addTo(mymap)
+                    directionsService.route(request, function(response, status) {
+                        if (status == 'OK') {
+                            directionsRenderer.setDirections(response)
+                        }
 
-                },
-                function(err) {
-                    console.error('Error', err)
-                }
-            )
-
+                    })
+                })
         })
 }
