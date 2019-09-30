81c852b37c989adcedaf50ce72adb1a39111ba6e
MattHeffNT <37504532+MattHeffNT@users.noreply.github.com>
2019-10-01 00:17:42 +0930

    Google Directions function now called and sort of working

--- scripts/details.js
+++ scripts/details.js
@@ -1,94 +1,97 @@
-function details() {
-    var url = 'https://open-darwin.opendata.arcgis.com/datasets/6d6453a83bbc4ab8b7591e545dd40d65_0.geojson'
-        // 'https://opendata.arcgis.com/datasets/f62cbfbf11494495984097ef8ed6a8a9_0.geojson'
+function details () {
+  var url = 'https://open-darwin.opendata.arcgis.com/datasets/6d6453a83bbc4ab8b7591e545dd40d65_0.geojson'
 
-    // Send request to server
-    fetch(url)
-        // Get JSON object from request
-        .then((response) => response.json())
-        .then((data) => {
-            var i = sessionStorage.getItem('key')
+  // Send request to server
+  fetch(url)
+  // Get JSON object from request
+    .then((response) => response.json())
+    .then((data) => {
+      var i = sessionStorage.getItem('key')
 
-            data.features[i].properties.Name
-                // if it matches, this specific item should be skipped
-            var vendorName = data.features[i].properties.BusinessName
-            var vendorLocation = data.features[i].properties.SiteName
-            var vendorWeHours = data.features[i].properties.WeekendHours
-            var vendorWdHours = data.features[i].properties.WeekdaysHours
-            var vendorPhHours = data.features[i].properties.PublicHolHours
-            var vendorWeb = data.features[i].properties.Website
-            var vendorLat = data.features[i].geometry[1]
-            var vendorLong = data.features[i].geometry[0]
-            var myLatLng = { lat: vendorLat, lng: vendorLong }
+      // put JSON properties into variables for easier use
+      var vendorName = data.features[i].properties.BusinessName
+      var vendorLocation = data.features[i].properties.SiteName
+      var vendorWeHours = data.features[i].properties.WeekendHours
+      var vendorWdHours = data.features[i].properties.WeekdaysHours
+      var vendorPhHours = data.features[i].properties.PublicHolHours
+      var vendorWeb = data.features[i].properties.Website
+      var vendorLat = data.features[i].geometry.coordinates[1]
+      var vendorLong = data.features[i].geometry.coordinates[0]
+      var vendorLatLng = { lat: vendorLat, lng: vendorLong }
 
-            document.querySelector('.display-4').innerHTML = `
+      document.querySelector('.display-4').innerHTML = `
                 ${vendorName}
                 `
-            document.getElementById('body-container').innerHTML = `<ul style="list-style:none;">
+      document.getElementById('body-container').innerHTML = `<ul style="list-style:none;">
                <li><strong> Location: </strong>${vendorLocation}</li>
                 <li><strong> Weekend Hours: </strong>${vendorWeHours}</li>
                 <li><strong> Weekday Hours: </strong>${vendorWdHours}</li>
-                <li><strong> Public Holidy Hours: </strong>${vendorPhHours}</li>
-                
+                <li><strong> Public Holidy Hours: </strong>${vendorPhHours}</li>            
                 <li style="text-align:center;"><a class="btn btn-primary" href=${vendorWeb} role="button">Website</a></li>
                 
-
                 </ul>
                 `
 
-            navigator.geolocation.watchPosition(
+      if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(
 
-                // get user location
-                function(position) {
-                    lat = position.coords.latitude
-                    long = position.coords.longitude
-                        // console.log(long)
+          Position,
+          displayError, 
+          { enableHighAccuracy: true }
 
-                    // directions API
+        )
+      } else {
+        window.alert('Geolocation not supported by this browser')
+      }
+      // get user location
+      function Position (position) {
+        lat = position.coords.latitude
+        long = position.coords.longitude
 
-                    var directionsService = new google.maps.DirectionsService()
-                    var directionsRenderer = new google.maps.DirectionsRenderer()
+        // directions API
 
-                    var origin = new google.maps.LatLng(lat, long) // place user location here
-                    var destination = new google.maps.LatLng(vendorLat, vendorLong) // vendor location
+        var origin = new google.maps.LatLng(lat, long) // place user location here
+        var destination = new google.maps.LatLng(vendorLat, vendorLong) // vendor location
 
-                    var mapOptions = {
-                        zoom: 14,
-                        center: origin,
-                        disableDefaultUI: true
-                    }
+        var directionsService = new google.maps.DirectionsService()
+        var directionsRenderer = new google.maps.DirectionsRenderer()
 
-                    var myLatLng = { lat: lat, lng: long };
+        var mapOptions = {
+          zoom: 14,
+          center: origin,
+          disableDefaultUI: true
+        }
 
+        var map = new google.maps.Map(document.getElementById('map'), mapOptions)
+        console.log("added map")
 
+        directionsRenderer.setMap(map)
 
-                    var map = new google.maps.Map(document.getElementById('map'), mapOptions)
-                    directionsRenderer.setMap(map)
+        var origin = new google.maps.LatLng({ lat: lat, lng: long }) // place user location here
+        var destination = new google.maps.LatLng({ lat: vendorLat, lng: vendorLong }) // vendor location
 
-                    var marker = new google.maps.Marker({
-                        position: myLatLng,
-                        map: map,
-                        title: 'User'
-                    })
-
-
-                    // var selectedMode = Driving
-
-                    var request = {
-                        origin: origin,
-                        destination: destination,
-                        // Note that Javascript allows us to access the constant
-                        // using square brackets and a string value as its
-                        // "property."
-                        travelMode: 'DRIVING'
-                    };
-
-                    directionsService.route(request, function(response, status) {
-                        if (status == 'OK') {
-                            directionsRenderer.setDirections(response)
-                        }
-
-                    })
-                })
-        })
+        calcRoute()
+        function calcRoute () {
+          var request = {
+            origin: origin,
+            destination: destination,
+            travelMode: 'DRIVING'
+          }
+          directionsService.route(request, function (result, status) {
+            if (status == 'OK') {
+              directionsRenderer.setDirections(result)
+            }
+          })
+        }
+      }
+    
+    function displayError (error) {
+      var errors = {
+        1: 'Permission denied',
+        2: 'Position unavailable',
+        3: 'Request timeout'
+      }
+      window.alert('Error: ' + errors[error.code] + ' you will need to enable location if you want to use this app to the full')
+    }
+  })
 }
