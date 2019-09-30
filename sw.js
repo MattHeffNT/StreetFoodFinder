773230324274cae86c0430827f3d1159ffe2d4dd
MattHeffNT <37504532+MattHeffNT@users.noreply.github.com>
2019-10-01 00:17:06 +0930

    removed details js and html from service worker for testing

--- sw.js
+++ sw.js
@@ -2,33 +2,32 @@ var CACHE_TITLE = 'my-site-cache'
 var CACHE_VERSION = 'v1'
 var CACHE_NAME = CACHE_TITLE + '-' + CACHE_VERSION
 var urlsToCache = [
-    'styles/main.css',
-    'scripts/javascript.js',
-    'index.html',
-    'details.html',
-    'scripts/details.js'
+  'styles/main.css',
+  'scripts/javascript.js',
+  'index.html'
+
 ]
 
-self.addEventListener('install', function(event) {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            console.log('Opened cache')
-            return cache.addAll(urlsToCache)
-        })
-    )
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache')
+        return cache.addAll(urlsToCache)
+      })
+  )
 })
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            // Cache hit - return response
-            if (response) {
-                return response
-            }
-            return fetch(event.request)
-        })
-    )
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        // Cache hit - return response
+        if (response) {
+          return response
+        }
+        return fetch(event.request)
+      })
+  )
 })
