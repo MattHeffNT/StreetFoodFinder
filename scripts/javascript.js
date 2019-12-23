d9af0d4548f669f991f1e60c933b51b66fd9ccbb
MattHeffNT <37504532+MattHeffNT@users.noreply.github.com>
2019-12-23 14:20:21 +0930

    Revert "moved card/swiper injection from this file to the index to try fix glitchy swiper"
    This reverts commit 4bb3669ea5a3040fcc3a96265745279d3e4476e9.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -21,17 +21,11 @@ function streetFood () {
   var url = 'https://open-darwin.opendata.arcgis.com/datasets/6d6453a83bbc4ab8b7591e545dd40d65_0.geojson'
 
   // Send request to server
-
   fetch (url)
-
 
     // Get JSON object from request
     .then((response) => response.json())
     .then((data) => {
-
-      // should be able to use array map here
-
-      
 
       for (let i = 0; i < data.features.length; i++) {
 
@@ -41,35 +35,56 @@ function streetFood () {
         // hacky way of getting rid of duplicate names, need to consider method of fixing the issue with Ken's Crepes Mindil as
         // the ' is incorrect.....
 
-       // console.log(vendor)
+
+        if (vendor == "Ken’s Crepes Mindil") {
+
+          var KCM = [i]
+
+          data.features[KCM].properties.BusinessName = "Ken's Crepes Mindil"
+
+        } else {
+
+          if (vendor == "Noi Pad Thai") {
+
+            var NPT = [i]
+
+            data.features[NPT].properties.BusinessName = "Noi Pad Thai"
+
+          } else {
+
+
+            // figure out how to add specific vendor images instead of stock
+            // horizontal swiping on mobile still not working.
+
 
             var vendorName = data.features[i].properties.BusinessName
-            const inject = document.querySelector('.swiper-wrapper').innerHTML +=
+            document.querySelector('#vendors').innerHTML +=
+
 
               `   
-                  <!-- Card -->
-                  <div class = "swiper-slide">
+                    <!-- Card -->
                     <div class="card">
-                        <!-- Card image -->
-                        <div class="view overlay">
-                        <!-- add Onclick add to session storage so as to record value of i for dynamic details page -->
-                        <img class="card-img-top" src=https://source.unsplash.com/collection/139608/544x362/?${[i]}" alt="Card image">
+                      <!-- Card image -->
+                      <div class="view overlay">
+                      <!-- add Onclick add to session storage so as to record value of i for dynamic details page -->
+                        <img class="card-img-top" src="https://source.unsplash.com/collection/139608/544x362/?${[i]}" alt="Card image">
                         <a href="./details.html" onclick = " sessionStorage.setItem('key', '${[i]}'); ">
                           <div class="mask rgba-white-slight"></div>
-                        </div>
-                        <!-- Card content -->
-                        <div class="card-body">
-                          <!-- Title -->
-                          <h4 class="card-title">${vendorName}</h4>
-                          <!-- Text -->
-                          <p class="card-text"></p>
-                        </div>
-                        </a>
+                        
+                      </div>
+                      <!-- Card content -->
+                      <div class="card-body">
+                        <!-- Title -->
+                        <h4 class="card-title">${vendorName}</h4>
+                        <!-- Text -->
+                        <p class="card-text"></p>
+                      </div>
+                      </a>
                     </div>
-                  </div>
-                    
                         `
+
           }
         }
-      )
-    }
+      }
+    })
+}
